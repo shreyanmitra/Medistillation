44ff7f56f2bf661f186641fa7de5b9e22b6582ff
BryanZhao <bohanz04@uw.edu>
2025-11-25 23:41:27 -0800

    Some more changes

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+python -m venv
+source./bin/activate
 pip install -r requirements.txt
-pip install plotly kaleido ipywidgets
+pip install plotly kaleido ipywidgets huggingface-hub pandas plotly
 python src/DataLoader.py --prepare_all
+python3 meddistillation_experiment.py
